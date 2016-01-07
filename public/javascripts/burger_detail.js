a63a3e94741e30644f776f0e24ed932e139bbb18
Katherine Ude <Katie.Ude@gmail.com>
2016-01-07 13:33:00 -0600

    burger detail modal displays WITH all the things

--- public/javascripts/burger_detail.js
+++ public/javascripts/burger_detail.js
@@ -2,7 +2,6 @@
 //
 // });
 
-
 function addBurgerDetails(data) {
   // object of burger specific data
   var burger = data[0];
@@ -22,8 +21,16 @@ function addBurgerDetails(data) {
   var compileMeatTemplate = meat_template(meat);
   $("#meat_list").append(compileMeatTemplate);
 
+  showBurgerDetails();
 };
-//
+
+function showBurgerDetails(){
+  console.log('Burgers Belong Here');
+  $("#burgerzzz").click(function() {
+    console.log('Clicks are fucking annoying');
+    $(".ui.modal").modal("show");
+  });
+}
 // console.log(location.burgers);
 //
 // for (var i = 0; i <= location.burgers; i++) {
