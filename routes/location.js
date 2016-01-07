495f39d4c1c461cbeb3b5025bb8e62e195d4462d
David Beeler <david.beeler@gmail.com>
2016-01-07 16:21:06 -0600

    updated the map workings

--- routes/location.js
+++ routes/location.js
@@ -84,6 +84,35 @@ router.get('/q/:id', function(req, res, next) {
   });
 });
 
+router.post('/q', function(req, res, next) {
+  console.log("==========");
+  console.log(req.body);
+  var search = req.body;
+  // var termSplit = req.params.id.split("&");
+  // for ( var phrase in termSplit ) {
+  //   var pairSplit = termSplit[phrase].split(':');
+  //   search[pairSplit[0]] = pairSplit[1];
+  // }
+  console.log("===============");
+  console.log("Received Search");
+  console.log(search);
+  model.find(search,function(err, locations) {
+    if (err) {
+      res.json(buildErrorResponse(err));
+    } else {
+      console.log("===============")
+      console.log("Results");
+      console.log(locations);
+      res.json({message: "it worked"});
+      // res.json(locations);
+      // console.log(locations);
+      // console.log("stringify now");
+      // console.log(JSON.stringify(locations));
+      // res.render('map', { mapData: JSON.stringify(locations) });
+    }
+  });
+});
+
 router.get('/map', function(req, res, next) {
   res.render('map');
 });
