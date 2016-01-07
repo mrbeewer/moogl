2bb6577f997198ab5d5300fafbf9150b91a3303f
Katherine Ude <Katie.Ude@gmail.com>
2016-01-07 12:44:18 -0600

    Burger specific view FUNCTIONING

--- public/javascripts/burger_detail.js
+++ public/javascripts/burger_detail.js
@@ -4,15 +4,24 @@
 
 
 function addBurgerDetails(data) {
+  // object of burger specific data
   var burger = data[0];
   var keys = Object.keys(burger);
-  console.log(burger)
-  console.log(burger.description);
-
+  // object of meat specific data
+  var meat = burger.meat[0];
+  var keys = Object.keys(meat);
+  console.log(meat);
+  // create underscore template for burger specific view
   var burger_view = $("#burger-template").html();
   var burger_template = _.template(burger_view);
   var compileTemplate = burger_template(burger);
   $("#location_burgers").append(compileTemplate);
+  // create underscore template for types of meat
+  var meat_view = $("#meat-template").html();
+  var meat_template = _.template(meat_view);
+  var compileMeatTemplate = meat_template(meat);
+  $("#meat_list").append(compileMeatTemplate);
+
 };
 //
 // console.log(location.burgers);
